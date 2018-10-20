3adb5ac91c8cb88cc57d179d91e60e28156a5cb2
NoSocksForYou <kaappo.raivio@gmail.com>
2018-10-20 17:23:15 +0300

    autocommit

--- EV3_omni_wheels_robot/ev3dev/delta-omni-wheels-formation-kinematics.py
+++ EV3_omni_wheels_robot/ev3dev/delta-omni-wheels-formation-kinematics.py
@@ -456,7 +456,7 @@ def on_message(client, userdata, msg):
 
   elif m.split(".")[0] == 'grab':
     print("received MQTT: grab")
-    driver.motor_grabber.run_to_abs_pos(position_sp=driver.motor_grabber.count_per_rot/6, speed_sp=driver.motor_grabber.count_per_rot)
+    driver.motor_grabber.run_to_abs_pos(position_sp=driver.motor_grabber.count_per_rot/-3, speed_sp=driver.motor_grabber.count_per_rot)
 
   elif m.split(".")[0] == 'release':
     print("received MQTT: release")
